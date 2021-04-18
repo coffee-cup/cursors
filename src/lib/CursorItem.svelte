6bdba16e366831a95dedb971f1d5347666f5a497
Jake Runzer <jakerunzer@gmail.com>
2021-04-18 15:39:14 -0700

    init commit

---
+++ src/lib/CursorItem.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let cursor: string = "";
+</script>
+
+<div
+  class="cursor-{cursor} bg-gray-100 py-8 px-4 font-medium flex items-center justify-center rounded-sm"
+  style="cursor: {cursor}"
+>
+  {cursor}
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import CursorItem from "$lib/CursorItem.svelte";
+
+  const cursors = [
+    "alias",
+    "all-scroll",
+    "auto",
+    "cell",
+    "context-menu",
+    "col-resize",
+    "copy",
+    "crosshair",
+    "default",
+    "e-resize",
+    "ew-resize",
+  ];
+</script>
+
+<main class="px-4 w-full">
+  <header class="mt-12 mb-8 max-w-sm mx-auto">
+    <h1 class="text-6xl text-pink-600 font-bold text-center mb-4">Cursors</h1>
+    <h2 class="text-gray-500 text-center">
+      List of all the possible cursors on the web <br /> Click to copy
+    </h2>
+  </header>
+
+  <div class="grid gap-1 grid-cols-2 md:grid-cols-4 max-w-3xl mx-auto">
+    {#each cursors as cursor, index}
+      <CursorItem {cursor} />
+    {/each}
+  </div>
+</main>
