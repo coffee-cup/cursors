39a098d9196c34bf991f54e1229e8998c0bc9777
Jake Runzer <jakerunzer@gmail.com>
2021-04-18 23:40:14 -0700

    open footer link in new tab

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,8 +1,10 @@
 <footer class="py-8 max-w-3xl mx-auto text-sm text-gray-500 text-center">
   <span
     >Made with <span class="text-red-500">♥</span> by
-    <a href="https://jakerunzer.com" class="hover:underline hover:text-red-600"
-      >Jake</a
+    <a
+      href="https://jakerunzer.com"
+      target="_blank"
+      class="hover:underline hover:text-red-600">Jake</a
     ></span
   >
 </footer>
